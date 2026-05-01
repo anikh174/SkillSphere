3cdcb78dc93f3574cb23c3f73fb230da5de15b2c
anikh174 <hossainanik174@gmail.com>
2026-05-01 09:56:54 +0600

    The banner section has been added.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Banner from "@/components/banner/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      
+      <Banner></Banner>
     </div>
   );
 }
